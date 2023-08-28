eda2cf6caffc9c938177b57d8bdc318b7ce37fc9
FQuental <felipedoquental@gmail.com>
2023-08-27 22:17:23 -0300

    studing

--- App.js
+++ App.js
@@ -5,10 +5,12 @@ import Cesta from './src/screens/Cesta';
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <StatusBar />
+    <SafeAreaView>   
+      <StatusBar />  
       <Cesta />
     </SafeAreaView>
   );
 }
 
+//SafeAreaView um jeito de usar o View para não invadir as bordas do IOS
+//StatusBar serve para usar a statusbar do android
--- src/screens/Cesta.js
+++ src/screens/Cesta.js
@@ -1,22 +1,23 @@
 import React from "react";
 import { Image, StyleSheet, Dimensions, Text } from 'react-native';
-import topo from '../../assets/topo.png';
 
-const width = Dimensions.get('screen').width;
+import topo from '../../assets/topo.png'; //importando a imagem para o codigo
+
+const width = Dimensions.get('screen').width; //largura da tela
 
 export default function Cesta(){
     return <>
-    <Image source={topo} />
-    <Text style={estilos.titulo}>Detalhe da Cesta</Text>
+    <Image source={topo}  style={estilos.top}/>
+    <Text style={estilos.title}>Detalhe da Cesta</Text>
     </>
 }
 
 const estilos = StyleSheet.create({
-    topo: {
+    top: {
         width: "100%",
         height: 578 / 768 * width,
     },
-    titulo: {
+    title: {
         width: "100%",
         position: "absolute",
         textAlign: "center",
@@ -24,6 +25,6 @@ const estilos = StyleSheet.create({
         lineHeight: 26,
         color: "white",
         fontWeight: "bold",
-        padding: 16
+        padding: 16,
     }
 });
